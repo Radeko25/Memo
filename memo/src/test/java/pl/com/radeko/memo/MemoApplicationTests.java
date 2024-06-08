b2651d4f7d2d841b70bec7dc0293b8233a18fa7c
Radeko25 <83910529+Radeko25@users.noreply.github.com>
2024-06-08 16:12:36 +0200

    Initial Commit
    Creating repository.

---
+++ memo/src/test/java/pl/com/radeko/memo/MemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.com.radeko.memo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
